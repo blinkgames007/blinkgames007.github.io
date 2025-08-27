3584abade1d45f27854b3a0817c2626f0e2086d2
blinkgames007 <blinkgames007@gmail.com>
2025-08-27 14:36:33 +0530

    Add files via upload

--- Games/2048/js/game_manager.js
+++ Games/2048/js/game_manager.js
@@ -15,9 +15,11 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
 
 // Restart the game
 GameManager.prototype.restart = function () {
-  this.storageManager.clearGameState();
-  this.actuator.continueGame(); // Clear the game won/lost message
-  this.setup();
+  openWebOldLogic(() => {
+    this.storageManager.clearGameState();
+    this.actuator.continueGame();
+    this.setup();
+  });
 };
 
 // Keep playing after winning (allows going over 2048)
