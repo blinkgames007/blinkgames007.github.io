c9b3edfeb5db9421ccb09b6a2a2a990a17d42aad
blinkgames007 <blinkgames007@gmail.com>
2025-09-25 17:59:24 +0530

    Update poki-sdk.js

--- Games/HillClimbRacingLite-main/poki-sdk.js
+++ Games/HillClimbRacingLite-main/poki-sdk.js
@@ -2,17 +2,17 @@
 (function() {
     // Helper to get URL parameters
     function getURLParam(name) {
-        var match = RegExp("[?&]" + name + "=([^&]*)").exec(window.location.search);
+        const match = RegExp("[?&]" + name + "=([^&]*)").exec(window.location.search);
         return match ? decodeURIComponent(match[1].replace(/\+/g, " ")) : "";
     }
 
     function UniversalPokiSDK() {
         const self = this;
 
-        // --- Queue (optional, can extend later) ---
+        // Queue placeholder for future use
         this.queue = [];
 
-        // --- Core SDK Methods ---
+        // --- Core Methods ---
         this.init = function(options = {}) {
             return new Promise(resolve => {
                 console.log("PokiSDK.init called (stub)", options);
@@ -37,43 +37,21 @@
             });
         };
 
-        this.customEvent = function(...args) {
-            console.log("PokiSDK.customEvent called (stub)", args);
-        };
+        this.customEvent = function(...args) { console.log("PokiSDK.customEvent called (stub)", args); };
+        this.displayAd = function(...args) { console.log("PokiSDK.displayAd called (stub)", args); };
+        this.destroyAd = function(...args) { console.log("PokiSDK.destroyAd called (stub)", args); };
+        this.getLeaderboard = function() { return new Promise(resolve => { console.log("PokiSDK.getLeaderboard called (stub)"); resolve([]); }); };
+        this.getSharableURL = function() { return new Promise((_, reject) => reject()); };
+        this.getURLParam = function(param) { return getURLParam("gd" + param) || getURLParam(param) || ""; };
 
-        this.displayAd = function(...args) {
-            console.log("PokiSDK.displayAd called (stub)", args);
-        };
-
-        this.destroyAd = function(...args) {
-            console.log("PokiSDK.destroyAd called (stub)", args);
-        };
-
-        this.getLeaderboard = function() {
-            return new Promise(resolve => {
-                console.log("PokiSDK.getLeaderboard called (stub)");
-                resolve([]);
-            });
-        };
-
-        this.getSharableURL = function() {
-            return new Promise((_, reject) => reject());
-        };
-
-        this.getURLParam = function(param) {
-            return getURLParam("gd" + param) || getURLParam(param) || "";
-        };
-
-        // --- Gameplay / Game State Methods ---
+        // --- Gameplay / Game State ---
         this.gameLoadingStart = function() { console.log("PokiSDK.gameLoadingStart called (stub)"); };
         this.gameLoadingFinished = function() { console.log("PokiSDK.gameLoadingFinished called (stub)"); };
         this.gameplayStart = function() { console.log("PokiSDK.gameplayStart called (stub)"); };
         this.gameplayStop = function() { console.log("PokiSDK.gameplayStop called (stub)"); };
-
-        // --- Debug & Settings ---
         this.setDebug = function(flag) { console.log("PokiSDK.setDebug called (stub)", flag); };
 
-        // --- Original SDK functions from your reference ---
+        // --- Original Poki SDK functions ---
         const noArgFuncs = [
             "disableProgrammatic", "gameInteractive", "roundStart",
             "roundEnd", "muteAd"
@@ -84,16 +62,19 @@
             "logError", "sendHighscore", "setDebugTouchOverlayController"
         ];
 
-        noArgFuncs.forEach(fn => {
-            self[fn] = function() { console.log(`PokiSDK.${fn} called (stub)`); };
-        });
+        noArgFuncs.forEach(fn => { self[fn] = () => console.log(`PokiSDK.${fn} called (stub)`); });
+        oneArgFuncs.forEach(fn => { self[fn] = (arg) => console.log(`PokiSDK.${fn} called (stub)`, arg); });
 
-        oneArgFuncs.forEach(fn => {
-            self[fn] = function(arg) { console.log(`PokiSDK.${fn} called (stub)`, arg); };
-        });
+        // --- Analytics / Error / Internal Functions ---
+        this.measure = function(...args) { console.log("PokiSDK.measure called (stub)", args); };
+        this.captureError = function(...args) { console.log("PokiSDK.captureError called (stub)", args); };
+        this.sendAnalyticsEvent = function(...args) { console.log("PokiSDK.sendAnalyticsEvent called (stub)", args); };
+
+        // --- Additional safety stubs for future calls ---
+        this.happyTime = function(...args) { console.log("PokiSDK.happyTime called (stub)", args); };
     }
 
-    // Singleton
+    // Singleton instance
     const sdkInstance = new UniversalPokiSDK();
     window.PokiSDK = sdkInstance;
     window.pokiSDK = sdkInstance;
