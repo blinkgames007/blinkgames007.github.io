1d4210a2cda844df715c7e8b63767a7bf48b109d
blinkgames007 <blinkgames007@gmail.com>
2025-10-11 17:41:45 +0530

    Update layout.js

--- layout.js
+++ layout.js
@@ -4,8 +4,8 @@ async function loadLayout() {
   try {
     // Load header and footer
     const [headerHTML, footerHTML] = await Promise.all([
-      fetch('header.html').then(r => r.text()),
-      fetch('footer.html').then(r => r.text())
+      fetch('https://blinkgames007.github.io/header.html').then(r => r.text()),
+      fetch('https://blinkgames007.github.io/footer.html').then(r => r.text())
     ]);
 
     // Insert into page
